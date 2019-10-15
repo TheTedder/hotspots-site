0eefb8a4bbbfeae7822990780c2c4a9e72af0941
Stephen Fang <safang99@gmail.com>
2019-10-15 15:11:47 -0400

    Add model associations and validations for location

---
+++ app/models/location.rb
@@ -0,0 +1,11 @@
+class Location < ApplicationRecord
+  belongs_to :user
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0, only_integer: true }, allow_nil: true
+  validates :password_protected, inclusion: { in: [true, false] }, allow_nil: true
+  validates :zip, numericality: { only_integer: true }, presence: true
+end

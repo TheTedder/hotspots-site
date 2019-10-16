cd4cc31719333e38283d2badf5c79f437e327e8a
WILL GLYNN <wglynn42@gmail.com>
2019-10-16 15:17:29 -0400

    added migrations

---
+++ db/migrate/20191016190325_change_rating_body_null_default.rb
@@ -0,0 +1,11 @@
+class ChangeRatingBodyNullDefault < ActiveRecord::Migration[5.2]
+  def up
+    change_column_null :reviews, :body, false
+    change_column_default :reviews, :body, ""
+  end
+
+  def down
+    change_column_null :reviews, :body, true
+    change_column_default :reviews, :body, nil
+  end
+end

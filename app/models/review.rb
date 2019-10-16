19d8c3a6f0d6645a3d0fc12fdb5fc7e13cf92c00
WILL GLYNN <wglynn42@gmail.com>
2019-10-16 15:16:10 -0400

    validate foreign keys

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,11 +1,9 @@
-class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+class Review < ApplicationRecord
 
   validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
   validates :speed_data, numericality: { greater_than: 0 }
+  validates :user_id, presence: true
+  validates :location_id, presence: true
 
   belongs_to :user
   belongs_to :location

9a7d4d2c7a7f64500a3775bc32a1d202545fc6d6
Tedder <25165500+TheTedder@users.noreply.github.com>
2019-10-16 13:53:41 -0400

    add location rating stub

--- app/models/location.rb
+++ app/models/location.rb
@@ -8,4 +8,9 @@ class Location < ApplicationRecord
   validates :price, numericality: { greater_than_or_equal_to: 0, only_integer: true }, allow_nil: true
   validates :password_protected, inclusion: { in: [true, false] }, allow_nil: true
   validates :zip, numericality: { only_integer: true }, presence: true
+
+  def average_rating
+    #stub
+    nil
+  end
 end

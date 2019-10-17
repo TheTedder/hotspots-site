a4dc3b18b0f52fbf9374af62e57fd8a8208e6d7f
WILL GLYNN <wglynn42@gmail.com>
2019-10-17 15:35:51 -0400

    added tests for location show tile

---
+++ app/javascript/react/components/LocationShowTile.test.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { mount } from 'enzyme'
+
+import LocationShowTile from './LocationShowTile'
+
+describe('<LocationShowTile />', () => {
+  it('should display all of the information for a single location', () => {
+    let locationData = {
+      name: 'Dunkin',
+      address: '3 Third Street',
+      address2: 'Boston, MA 01234',
+      rating: 5,
+      price: '$12',
+      password_protected: 'yes'
+    }
+    const wrapper = mount(<LocationShowTile locationData={locationData} />)
+    expect(wrapper.findWhere(node => node.text().includes('Dunkin')).exists()).toBe(true)
+    expect(wrapper.findWhere(node => node.text().includes('3 Third Street')).exists()).toBe(true)
+    expect(wrapper.findWhere(node => node.text().includes('Boston, MA 01234')).exists()).toBe(true)
+    expect(wrapper.findWhere(node => node.text().includes('$12')).exists()).toBe(true)
+    expect(wrapper.findWhere(node => node.text().includes('yes')).exists()).toBe(true)
+  })
+})

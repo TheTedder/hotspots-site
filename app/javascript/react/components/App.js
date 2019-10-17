6bc777393a510fd2815bebed1020d7d1b945c485
Stephen Fang <safang99@gmail.com>
2019-10-17 09:48:30 -0400

    revert for plain react commit

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -1,13 +1,10 @@
 import React from 'react'
 import { Route, Switch, BrowserRouter } from "react-router-dom"
 
-import LocationsIndexContainer from "./LocationsIndexContainer"
-
 export const App = (props) => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={LocationsIndexContainer} />
       </Switch>
     </BrowserRouter>
   )

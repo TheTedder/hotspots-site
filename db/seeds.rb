e6b77745fb78dc2cb5cfe1674da44425850f8315
Stephen Fang <safang99@gmail.com>
2019-10-17 15:17:27 -0400

    removed seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-john = User.create(name: "John", email: "notMyDog@hotmail.com", password: "123456")
-
-Location.create(name: "cafe", address: "12 back st", city: "Boston", state: "MA", price: 10, password_protected: true, zip: "01222", user: john)
-
-Location.create(name: "Nero", address: "12 back st", city: "Cambridge", state: "MA", price: 10, password_protected: false, zip: "01222", user: john)
-
-Location.create(name: "Roche bros", address: "12 back st", city: "Boston", state: "MA", price: 10, password_protected: false, zip: "01222", user: john)
-
-Location.create(name: "BPL", address: "12 back st", city: "Boston", state: "MA", password_protected: false, zip: "01222", user: john)

63d394e7d176bf3fe64ba69a7e1452309c1b7876
WILL GLYNN <wglynn42@gmail.com>
2019-10-17 10:06:54 -0400

    Imported Location Show Page into App.js

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Route, Switch, BrowserRouter } from "react-router-dom"
 
+import LocationShowPage from './LocationShowPage'
+
 export const App = (props) => {
   return (
     <BrowserRouter>

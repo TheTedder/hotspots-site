2babbae37ccf3469e8faabce51a87493307412c8
Stephen Fang <safang99@gmail.com>
2019-10-18 14:52:01 -0400

    saving part of form

---
+++ app/javascript/react/components/ReviewForm.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+const ReviewForm = (props) => {
+
+  return(
+    <div className="callout primary">
+      <form>
+
+      </form>
+    </div>
+  )
+}
+
+export default ReviewForm

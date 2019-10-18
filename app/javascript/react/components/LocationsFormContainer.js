3a95d1a836f60f36dcb1d4b7ba21403cc7882762
OThayer <oliverthayer01@gmail.com>
2019-10-18 10:43:39 -0400

    updated new page with form

--- app/javascript/react/components/LocationsFormContainer.js
+++ app/javascript/react/components/LocationsFormContainer.js
@@ -7,7 +7,7 @@ const LocationsFormContainer = props => {
     city: "",
     state: "",
     zip: "",
-    passwordProtected: ""
+    password_protected: ""
   })
 
   const handleChange = (event) => {
@@ -18,33 +18,33 @@ const LocationsFormContainer = props => {
   }
 
   return(
-    <div id="newForm">
+    <div id="new-form">
       <h1>Add a new Hotspot!</h1>
         <form className="callout">
           <label htmlFor="name">
             Location Name:
-          <input type="text" id="name" />
+          <input type="text" id="name" name="name" value="name" onChange={handleChange}/>
           </label>
           <label htmlFor="address">
             Address:
-            <input type="text" id="address" />
+            <input type="text" id="address" name="address" value="address" onChange={handleChange}/>
           </label>
           <label htmlFor="city">
             City:
-            <input type="text" id="city" />
+            <input type="text" id="city" name="city" value="city" onChange={handleChange}/>
           </label>
           <label htmlFor="state">
             State:
-            <input type="text" id="state" />
+            <input type="text" id="state" name="state" value="state" onChange={handleChange}/>
           </label>
           <label htmlFor="zip">
             Zip Code:
-            <input type="text" id="zip" />
+            <input type="text" id="zip" name="zip" value="zip" onChange={handleChange}/>
           </label>
-          <label htmlFor="password_protected">
+          <label htmlFor="password-protected">
             Password Protected?
-          <select id="passwordProtected">
-            <option selected value=""></option>
+          <select id="password-protected" name="password-protected" value="password_protected" onChange={handleChange}>
+            <option defaultValue=""></option>
             <option value="true">Yes</option>
             <option value="false">No</option>
           </select>

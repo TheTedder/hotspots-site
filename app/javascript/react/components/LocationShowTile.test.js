2ff2d18d4500f363f8ad16afc56a46e45c25c949
WILL GLYNN <wglynn42@gmail.com>
2019-10-18 10:34:25 -0400

    refactored tests for location show tile

--- app/javascript/react/components/LocationShowTile.test.js
+++ app/javascript/react/components/LocationShowTile.test.js
@@ -4,28 +4,42 @@ import { mount } from 'enzyme'
 import LocationShowTile from './LocationShowTile'
 
 describe('<LocationShowTile />', () => {
-  it('should display all of the information for a single location', () => {
+  let wrapper
 
-    let locationData = {
-      name: 'Dunkin',
-      address: '3 Third Street',
-      address2: 'Boston, MA 01234',
-      rating: 5,
-      price: '$12',
-      password_protected: 'yes'
-    }
-    const wrapper = mount(<LocationShowTile
-      name="Dunkin"
-      address="3 Third Street"
-      address2="Boston, MA 01234"
-      rating={5}
-      price="$12"
-      password_protected="yes"
-    />)
-    expect(wrapper.findWhere(node => node.text().includes('Dunkin')).exists()).toBe(true)
-    expect(wrapper.findWhere(node => node.text().includes('3 Third Street')).exists()).toBe(true)
-    expect(wrapper.findWhere(node => node.text().includes('Boston, MA 01234')).exists()).toBe(true)
-    expect(wrapper.findWhere(node => node.text().includes('$12')).exists()).toBe(true)
-    expect(wrapper.findWhere(node => node.text().includes('yes')).exists()).toBe(true)
+  beforeEach(() => {
+    wrapper = mount(
+      <LocationShowTile
+        name="Dunkin"
+        address="3 Third Street"
+        address2="Boston, MA 01234"
+        rating={5}
+        price="$12"
+        password_protected="yes"
+      />
+    )
+  })
+
+  it("renders a h2 tag with the location name", () => {
+    expect(wrapper.find('h2#show-name').text()).toEqual("Name: Dunkin")
+  })
+
+  it("renders a h3 tag with the rating", () => {
+    expect(wrapper.find('h3#show-rating').text()).toEqual("Rating: 5")
+  })
+
+  it("renders a p tag with the address", () => {
+    expect(wrapper.find('p#show-address1').text()).toEqual("3 Third Street")
+  })
+
+  it("renders a p tag with the city, state, zip", () => {
+    expect(wrapper.find('p#show-address2').text()).toEqual("Boston, MA 01234")
+  })
+
+  it("renders a p tag with the price", () => {
+    expect(wrapper.find('p#show-price').text()).toEqual("Price: $12")
+  })
+
+  it("renders a p tag with the password protected status", () => {
+    expect(wrapper.find('p#show-password-protect').text()).toEqual("Password Protected: yes")
   })
 })

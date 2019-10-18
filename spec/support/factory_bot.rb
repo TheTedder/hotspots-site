92e1896cbc91dec0d4cc2e80253f60fd2121e74d
Tedder <25165500+TheTedder@users.noreply.github.com>
2019-10-18 10:17:29 -0400

    fakerized user factory bot

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -1,8 +1,9 @@
 require 'factory_bot'
+require 'faker'
 
 FactoryBot.define do
   factory :user do
-    name { 'Jonathan' }
+    name { Faker::Name.first_name }
     sequence(:email) {|n| "user#{n}@example.com" }
     password { 'password' }
     password_confirmation { 'password' }

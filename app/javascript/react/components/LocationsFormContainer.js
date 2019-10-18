ebd0e587402eef6c8a7c96270e276f23c1a495e4
OThayer <oliverthayer01@gmail.com>
2019-10-18 11:03:09 -0400

    updated new page with form with all fields

--- app/javascript/react/components/LocationsFormContainer.js
+++ app/javascript/react/components/LocationsFormContainer.js
@@ -7,7 +7,8 @@ const LocationsFormContainer = props => {
     city: "",
     state: "",
     zip: "",
-    password_protected: ""
+    password_protected: "",
+    price: ""
   })
 
   const handleChange = (event) => {
@@ -23,32 +24,36 @@ const LocationsFormContainer = props => {
         <form className="callout">
           <label htmlFor="name">
             Location Name:
-          <input type="text" id="name" name="name" value="name" onChange={handleChange}/>
+          <input type="text" id="name" name="name" value={newLocation.name} onChange={handleChange}/>
           </label>
           <label htmlFor="address">
             Address:
-            <input type="text" id="address" name="address" value="address" onChange={handleChange}/>
+            <input type="text" id="address" name="address" value={newLocation.address} onChange={handleChange}/>
           </label>
           <label htmlFor="city">
             City:
-            <input type="text" id="city" name="city" value="city" onChange={handleChange}/>
+            <input type="text" id="city" name="city" value={newLocation.city} onChange={handleChange}/>
           </label>
           <label htmlFor="state">
             State:
-            <input type="text" id="state" name="state" value="state" onChange={handleChange}/>
+            <input type="text" id="state" name="state" value={newLocation.state} onChange={handleChange}/>
           </label>
           <label htmlFor="zip">
             Zip Code:
-            <input type="text" id="zip" name="zip" value="zip" onChange={handleChange}/>
+            <input type="text" id="zip" name="zip" value={newLocation.zip} onChange={handleChange}/>
           </label>
           <label htmlFor="password-protected">
             Password Protected?
-          <select id="password-protected" name="password-protected" value="password_protected" onChange={handleChange}>
+          <select id="password-protected" name="password-protected" value={newLocation.password_protected} onChange={handleChange}>
             <option defaultValue=""></option>
             <option value="true">Yes</option>
             <option value="false">No</option>
           </select>
-        </label>
+          </label>
+          <label htmlFor="price">
+            Price:
+            <input type="text" id="price" name="price" value={newLocation.price} onChange={handleChange}/>
+          </label>
         <input type="submit" value="Submit" />
       </form>
     </div>

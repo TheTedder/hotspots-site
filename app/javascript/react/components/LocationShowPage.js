ec88c873a0042dcb8f6863f0d96deb7a16587a2c
Stephen Fang <safang99@gmail.com>
2019-10-18 15:24:35 -0400

    changed underscore to camelcase

--- app/javascript/react/components/LocationShowPage.js
+++ app/javascript/react/components/LocationShowPage.js
@@ -57,7 +57,7 @@ const LocationShowPage = props => {
       address={locationData.address}
       address2={locationData.address2}
       price={locationData.price}
-      password_protected={locationData.password_protected}
+      passwordProtected={locationData.password_protected}
     />
   )
 }

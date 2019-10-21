bfce191b8f2e4bf30367d9844f84bfd7761ff244
WILL GLYNN <wglynn42@gmail.com>
2019-10-21 09:43:22 -0400

    resolved routes conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
   root 'homes#index'
   get '/locations', to: 'homes#index'
-<<<<<<< HEAD
-  get '/locations/:id', to: 'homes#index'
-  get '/locations/new', to: 'homes#index'
-=======
   get '/locations/new', to: 'homes#index'
   get '/locations/:id', to: 'homes#index'
->>>>>>> c5747f215029ae63d78db21ddb6d58765fd57448
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do

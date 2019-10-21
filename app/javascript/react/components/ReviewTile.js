e0d936c9f2db30c9b2d821d2f548b4e0d0efaeb0
WILL GLYNN <wglynn42@gmail.com>
2019-10-21 13:45:34 -0400

    added star color classes

--- app/javascript/react/components/ReviewTile.js
+++ app/javascript/react/components/ReviewTile.js
@@ -1,9 +1,28 @@
 import React from 'react'
 
 const ReviewTile = props => {
+  let stars = []
+
+  for (let i = 0; i < 5; i++){
+    let starClass = 'fa fa-star '
+    if (i < props.rating){
+      starClass += 'gold-star'
+    } else{
+      starClass += 'grey-star'
+    }
+
+    stars.push(
+      <i className={starClass} key={i}></i>
+    )
+  }
   return (
     <div className="review-tile row">
-
+      <div className="row rating">
+        Rating: {stars}
+      </div>
+      <div className="review-tile-body row">
+        {props.body}
+      </div>
     </div>
   )
 }

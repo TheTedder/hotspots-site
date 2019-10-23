0798b7d244d2e1bb06c08c1f7e421eab4da7505d
Tedder <25165500+TheTedder@users.noreply.github.com>
2019-10-23 14:42:19 -0400

    add photo ref column to locations table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_18_171241) do
+ActiveRecord::Schema.define(version: 2019_10_23_182024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2019_10_18_171241) do
     t.boolean "password_protected"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo_ref"
     t.index ["user_id"], name: "index_locations_on_user_id"
   end
 

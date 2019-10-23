630020023ba07145d08bfce4ad0da94d27e37298
Stephen Fang <safang99@gmail.com>
2019-10-23 13:13:40 -0400

    final fix for create review path

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,12 +1,8 @@
 require 'JSON'
 class Api::V1::ReviewsController < ApiController
-  protect_from_forgery unless: -> { request.format.json?}
-
   def create
-    binding.pry
-    # authenticity token creates spaces on "+", so sometimes it wont correctly read that a user has signed in.
     location = Location.find(params["location_id"])
-    new_review = Review.new(JSON.parse(request.body.read)["review"])
+    new_review = Review.new(review_params)
     new_review.user = current_user
     new_review.location = location
     new_review.save
@@ -16,7 +12,5 @@ class Api::V1::ReviewsController < ApiController
   private
   def review_params
     params.require(:review).permit(:rating, :body, :speed_data)
-    # params.permit!
-    # params.require(:review).permit!
   end
 end
--- app/javascript/react/components/LocationShowPage.js
+++ app/javascript/react/components/LocationShowPage.js
@@ -19,7 +19,6 @@ const LocationShowPage = props => {
     }
   )
 
-
   useEffect(() => {
     fetch(`/api/v1/locations/${props.match.params.id}`)
     .then(response => {
@@ -55,44 +54,9 @@ const LocationShowPage = props => {
       setReviews(json.location.reviews)
     })
   }, [])
-  //
-  // const onReviewSubmitted = (newReview, formClear) => {
-  //   let token = document.getElementsByName("csrf-token")[0].content
-  //   let fixedToken = encodeURIComponent(token)
-  //   fetch(`/api/v1/locations/${props.match.params.id}/reviews?authenticity_token=${fixedToken}`, {
-  //     method: 'POST',
-  //     credentials: "same-origin",
-  //     header: {
-  //         'Accept': 'application/json',
-  //         'Content-Type': 'application/json'
-  //     },
-  //     body: JSON.stringify(newReview)
-  //   })
-  //   .then(response => {
-  //     if (response.ok) {
-  //       return response;
-  //     } else {
-  //       let errorMessage = `${response.status} (${response.statusText})`,
-  //        error = new Error(errorMessage);
-  //       throw(error);
-  //     }
-  //   })
-  //   .then(response => response.json())
-  //   .then(reviewBody => {
-  //     if (reviewBody.review.error_messages.length === 0) {
-  //       formClear()
-  //       setReviews([...reviews, reviewBody.review])
-  //     } else {
-  //       setErrorList(reviewBody.review.error_messages)
-  //     }
-  //   })
-  //   .catch(error => console.error(`Error in fetch: ${error.message}`))
-  //   }
 
   const onReviewSubmitted = (newReview, formClear) => {
-    // let token = document.getElementsByName("csrf-token")[0].content
-    // let fixedToken = encodeURIComponent(token)
-    fetch(`/api/v1/locations/${props.match.params.id}/reviews.json`, {
+    fetch(`/api/v1/locations/${props.match.params.id}/reviews`, {
       method: 'POST',
       credentials: "same-origin",
       headers: {

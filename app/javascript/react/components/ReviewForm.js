1302db30986ade9e95cb70c16229b2c0281dfa9f
OThayer <oliverthayer01@gmail.com>
2019-10-24 18:29:42 -0400

    button colors and sizing

--- app/javascript/react/components/ReviewForm.js
+++ app/javascript/react/components/ReviewForm.js
@@ -59,27 +59,13 @@ const ReviewForm = (props) => {
           <h3>Add a new review!</h3>
           <form onSubmit={onSubmitHandler}>
             {stars}
-            <label htmlFor="body">
-              Description:
-            </label>
             <textarea
               name="body"
               id="body"
               value={newReview.body}
               onChange={handleChange}
+              placeholder="Type Your Review"
             />
-
-          <div className="row">
-            <div className="large-12 cell">
-              <textarea
-                name="body"
-                id="body"
-                value={newReview.body}
-                onChange={handleChange}
-                placeholder="Type Your Review"
-              />
-            </div>
-          </div>
 
             <input type="submit" value="Submit" />
           </form>

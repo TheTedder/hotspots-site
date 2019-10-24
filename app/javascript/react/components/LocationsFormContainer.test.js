d96d10f80493ac0e1ecb89ebb61640450344120c
Stephen Fang <safang99@gmail.com>
2019-10-24 13:44:24 -0400

    started testing

---
+++ app/javascript/react/components/LocationsFormContainer.test.js
@@ -0,0 +1,7 @@
+import React from "react"
+import Enzyme, { mount } from "enzyme"
+import Adapter from "enzyme-adapter-react-16"
+import { BrowserRouter } from 'react-router-dom'
+Enzyme.configure({ adapter: new Adapter() })
+
+import LocationsFormContainer from "./LocationsFormContainer"

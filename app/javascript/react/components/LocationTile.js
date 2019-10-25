a4a0f79f11a169a7f09dbe771521dca476b540cc
Stephen Fang <safang99@gmail.com>
2019-10-24 21:45:16 -0400

    added new location button on index

--- app/javascript/react/components/LocationTile.js
+++ app/javascript/react/components/LocationTile.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const LocationTile = props => {
   let lockIcon
-  if ('passwordProtected' in props){
+  if (props.passwordProtected !== null){
     let lockIconClass = 'fa fa-lock'
     if (!props.passwordProtected){
       lockIconClass += '-open'

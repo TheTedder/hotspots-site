572505c5adb1c88ee99c017fce8d636b04120078
Stephen Fang <safang99@gmail.com>
2019-10-24 20:02:56 -0400

    images for show page

--- app/jobs/fetch_image_job.rb
+++ app/jobs/fetch_image_job.rb
@@ -22,6 +22,7 @@ class FetchImageJob < ApplicationJob
       response = Net::HTTP.get_response(uri)
       next unless Net::HTTPSuccess === response
       place = JSON.parse(response.body)
+      binding.pry
       next if place['status'] == 'ZERO_RESULTS'
       photoref = place['candidates'].first['photos'].first['photo_reference']
       next if photoref.nil?
